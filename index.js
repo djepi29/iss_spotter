1619e3a6d092cf3d4f488fe0fe00cefa2fc33f8d
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-02-24 20:54:41 -0500

    refactor with promises

--- index.js
+++ index.js
@@ -42,14 +42,7 @@
 
 const { nextISSTimesForMyLocation } = require('./iss');
 
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+const printPassTimes = require("./printPassTimes");
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
---
+++ index2.js
@@ -0,0 +1,22 @@
+// const { 
+//   fetchMyIP,
+//   fetchCoordsByIP,
+//   fetchISSFlyOverTimes,
+//   } = require('./iss_promised');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = require("./printPassTimes");
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
